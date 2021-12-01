ff88d8edc52b243106270e1445f1a75bfbeaa571
Marcus Widegren <marcuswidegren123@gmail.com>
2021-12-01 06:05:57 +0100

    Added first day

--- src/main/java/dayone/First.kt
+++ src/main/java/dayone/First.kt
@@ -3,5 +3,16 @@ package dayone
 import util.FileReader
 
 fun main() {
-    FileReader().readFile("dayone/input.txt").forEach { println(it) }
+    var count = 0
+    val lines = FileReader().readFile("dayone/input.txt").map { it.toInt() }
+
+    for (i in 1 until lines.size) {
+        val last = lines[i - 1]
+        val now = lines[i]
+        if (last < now) {
+            count++
+        }
+    }
+
+    println(count)
 }
--- src/main/java/dayone/Second.kt
+++ src/main/java/dayone/Second.kt
@@ -1,2 +1,23 @@
 package dayone
 
+import util.FileReader
+
+fun main() {
+    var count = 0
+    val lines = FileReader().readFile("dayone/input.txt").map { it.toInt() }
+
+    for (i in 3 until lines.size) {
+        val last1 = lines[i - 3]
+        val last2 = lines[i - 2]
+        val last3 = lines[i - 1]
+        val now = lines[i]
+
+        val lastMeasurement = last1 + last2 + last3
+        val currentMeasurement = last2 + last3 + now
+        if (lastMeasurement < currentMeasurement) {
+            count++
+        }
+    }
+
+    println(count)
+}
